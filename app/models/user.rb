03c570571bf09731bf1606bd79b99e4932df120e
AM <alexmarco18795@gmail.com>
2022-04-09 19:26:39 +0100

    :sparkles: Allow users to sign in with nickname
    - allow only alphanumeric nicknames
    - introduce nickname on signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true, uniqueness: true
+  validates :nickname, presence: true, uniqueness: { case_sensitive: false }
+  validates_format_of :nickname, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+
+  attr_writer :login
+
+  # Allow users to sign in using either email or nickname
+  def login
+    @login || self.nickname || self.email
+  end
+
+  # Overwrite Devise's method to change login action behaviour
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions.to_h).where(["lower(nickname) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    elsif conditions.has_key?(:nickname) || conditions.has_key?(:email)
+      where(conditions.to_h).first
+    end
+  end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,15 +11,31 @@ RSpec.describe User, type: :model do
     expect(user).to be_valid
   end
 
-  it 'shouldnt allow a duplicate alias' do
+  it 'shouldnt allow a duplicate nickname, even using caps' do
     user = create(:user, nickname: 'ted', email: 'ted1@test.com')
     expect(user).to be_valid
-    user = build(:user, email: 'tes2@test.com', nickname: 'ted')
+    user = build(:user, nickname: 'ted', email: 'tes2@test.com')
+    expect(user).to_not be_valid
+    user = build(:user, nickname: 'Ted', email: 'tes2@test.com')
     expect(user).to_not be_valid
   end
 
   it 'should always require a nickname' do
     user = build(:user, nickname: nil)
     expect(user).to_not be_valid
+  end
+
+  it 'shouldnt allow a nickname that looks like an email or contains other special chars' do
+    user = build(:user, nickname: 'ender@captain', email: 'ender@test.com')
+    expect(user).to_not be_valid
+    user = build(:user, nickname: 'enderc!123', email: 'ender@test.com')
+    expect(user).to_not be_valid
+  end
+
+  it 'should allow nicknames with numbers, underscores or dots' do
+    user = build(:user, nickname: 'ender_captain', email: 'ender@test.com')
+    expect(user).to be_valid
+    user = build(:user, nickname: 'e.c.123', email: 'ender@test.com')
+    expect(user).to be_valid
   end
 end
