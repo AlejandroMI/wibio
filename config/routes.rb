719ce94aa30a4f4d3a8e4f7cedb2c0f7fa8b2a22
AM <alexmarco18795@gmail.com>
2022-04-25 09:10:42 +0100

    :construction: edit user page
    - change some tw styles
    - abstract label into css class
    - change edit page for registrations
    - refactor primary button to cta
    - fix problem cancelling account
    - change body to lightest gray
    
    Next:
    
    - finish page
    - add user controller
    - add another section for password and email

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   get 'bio/edit'
-  devise_for :users
+
+  Rails.application.routes.draw do
+    devise_scope :user do
+      # Redirests signing out users back to sign-in
+      get "users", to: "devise/sessions#new"
+    end
+    devise_for :users
+  end
 
   authenticated :user do
     root 'bio#edit', as: :authenticated_root
