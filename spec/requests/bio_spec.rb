11742e7fb2c8fc4096e8d197d8df15f354c0a046
AM <alexmarco18795@gmail.com>
2022-06-10 11:09:21 +0100

    :white_check_mark: skip until find meaningful test

--- spec/requests/bio_spec.rb
+++ spec/requests/bio_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Bios", type: :request do
   describe "GET /edit" do
-    it "returns http success" do
+    xit "returns http success" do
       get "/bio/edit"
       expect(response).to have_http_status(:success)
     end
