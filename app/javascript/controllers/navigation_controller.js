66623ef5bd3b29727a10d9d38c317f4acfe5c2c7
AM <alexmarco18795@gmail.com>
2022-06-10 17:57:51 +0100

    :wheelchair: make nav menu accesible

--- app/javascript/controllers/navigation_controller.js
+++ app/javascript/controllers/navigation_controller.js
@@ -3,15 +3,9 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
     static targets = [ "mobileMenu", "open", "close" ]
 
-    toggle(event) {
-        if (event.target == this.openTarget || event.target.closest('span') == this.openTarget) {
-            this.open(event)
-        } else {
-            this.close(event)
-        }
-    }
-
     open(event) {
+        if (!this.clicked_or_enter(event)) { return }
+
         // Toggle buttons
         this.openTarget.classList.add('hidden')
         this.closeTarget.classList.remove('hidden')
@@ -20,6 +14,7 @@ export default class extends Controller {
         setTimeout(() => {
             this.mobileMenuTarget.classList.remove('hidden');
             this.mobileMenuTarget.classList.add('transform', 'ease-out', 'duration-300', 'transition', '-translate-y-2', 'opacity-0');
+            this.closeTarget.focus()
 
             // Trigger transition
             setTimeout(() => {
@@ -29,6 +24,8 @@ export default class extends Controller {
     }
 
     close(event) {
+        if (!this.clicked_or_enter(event)) { return }
+
         // Toggle buttons
         this.closeTarget.classList.add('hidden')
         this.openTarget.classList.remove('hidden')
@@ -39,12 +36,15 @@ export default class extends Controller {
         // Trigger transition
         setTimeout(() => {
             this.mobileMenuTarget.classList.add('opacity-0', '-translate-y-2');
+            this.openTarget.focus()
             // Hide element after transition
             setTimeout(() => {
                 this.mobileMenuTarget.classList.add('hidden');
             }, 100);
         }, 50);
+    }
 
-
+    clicked_or_enter(event) {
+        return event.type === "click" || event.keyCode === 13
     }
 }
