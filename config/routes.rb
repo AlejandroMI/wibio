31292761153fee429c21c4ad10e5c6835690e6bd
AM <alexmarco18795@gmail.com>
2023-02-02 12:03:02 +0100

    :truck: Move all app resources under /a scope
    This is to avoid the clashes of a name like: links with the resource links. I have written a test that checks the route file for any routes created outside of this scope or the default ones that rails/gems have.

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,24 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  # Active Admin
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   # Set devise for users, customizing through registration controller and with prefix to avoid collisions with user paths
   devise_for :users, controllers: { registrations: :registrations }, path_prefix: "d"
 
-  resources :links
-
   # This is the main page for each user and their links
   get ":nickname", to: "users#page", as: :wibio_page
 
-  resources :users, only: [:update]
-  get "welcome", to: "users#welcome"
-  post "finish_welcome", to: "users#finish_welcome"
+  # All application routes
+  # We isolate the application to /a to avoid collisions with user wibio pages
+  scope "/a" do
+    resources :links
+    resources :users, only: [:update]
+    get "welcome", to: "users#welcome"
+    post "finish_welcome", to: "users#finish_welcome"
+  end
 
   # This should go always before the root page
   authenticated :user do
