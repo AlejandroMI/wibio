1e956822feb5251d71fdc49730f060353cb328e7
AM <alexmarco18795@gmail.com>
2023-03-08 09:56:45 +0100

    🧪 Add pending test for signout feature
    I am having problem with hidden elements so my theory is that CSS is not being loaded properly. Need to fix capybara so I can take screenshots and check if styles are loaded. On a plane so need internet to get some gems.

---
+++ spec/features/user_signout_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+require 'support/features_helpers'
+
+RSpec.feature 'User Sign Out', type: :feature do
+  pending "add some examples to (or delete) #{__FILE__}"
+
+  # include FeaturesHelpers
+  #
+  # scenario 'user can sign out' do
+  #   user = create(:user, :confirmed)
+  #   login(user)
+  #   click_button 'Open menu'
+  # end
+end
